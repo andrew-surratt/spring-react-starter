2d9a7ffcd448698e11e89a68739cd3b154445305
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2024-07-24 20:10:20 -0400

    Setup prettier

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,27 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.app.json',
+    project: "./tsconfig.app.json",
   },
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
   settings: {
     react: {
-      version: 'detect',
-    }
-  }
-}
+      version: "detect",
+    },
+  },
+};
